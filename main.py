ee8414b0ffa4bb203c6e56eeb88d02b91985e7b2
Cathal Butler <cathalbutler@outlook.com>
2020-04-02 02:27:28 +0100

    Updated text file name, implemented query_dictionary and exec_command functions

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 import subprocess
 
 
-def capture_from_audio(recogniser, audio_input, offset, duration, adjust_ambient_duration, display_transcript):
+def process_audio_file(recogniser, audio_input, offset, duration, adjust_ambient_duration, display_transcript):
     """Capture Data From File"""
     """Offset and Duration useful for segmenting audio if have prior knowledge of audio file"""
     # Check if Recogniser is valid type
@@ -37,7 +37,7 @@ def capture_from_audio(recogniser, audio_input, offset, duration, adjust_ambient
         print('ERROR: Could not recognise audio\nPlease check input and try agian...')
 
 
-def capture_from_mic(recogniser, mic_input, application_dic):
+def capture_from_mic(recogniser, mic_input):
     """Capture Data From Microphone"""
     if not isinstance(mic_input, sr.Microphone):
         raise TypeError("'mic' must be an instance of `Microphone`")
@@ -48,60 +48,100 @@ def capture_from_mic(recogniser, mic_input, application_dic):
         audio_recognised = recogniser.recognize_google(audio)
         audio_recognised.lower()
         print('Recognised: ', audio_recognised.lower())
-        print(application_dic)
-
-        # Read key and value from dic
-        for k, val in application_dic.items():
-            if audio_recognised.lower() == val:
-                print('Opening ', k)
-                process = subprocess.Popen(k)
+        return audio_recognised
 
     except Exception as e:
-        print(e)
-        print(f'ERROR: Could not recognise audio\nPlease check input and try agian...\n{e}')
-
-    return audio_recognised
+        print(f'ERROR: Could not recognise audio\nPlease check input and try again...\n{e}')
 
 
-def read_application_file():
-    print('ready file')
-    file = 'application_list.txt'
+def read_command_file():
+    print('Reading file...')
+    command_file_path = 'command_dic.txt'
+    dic = {}
 
-    with open(file) as f:
-        app_list_dic = dict(x.rstrip().split(None, 1) for x in f)
+    # Get k and val from file:
+    if os.path.exists(command_file_path):
+        with open(command_file_path, 'r') as f:
+            try:
+                for line in f:
+                    data = line.split()
+                    if len(data) == 2:
+                        key, value = data[0], data[1]
+                        dic[key] = value
 
-    return app_list_dic
+                print(dic)
+                return dic
+            except:
+                print('Error processing file')
 
 
-def main_menu():
-    print('===================================================='
-          '\n========== Application Speech Command =============='
-          '\n====================================================\n')
+# Function that loops though commands_dictionary
+# to find a match to the query
+# Functions returns the key which is the command
+def query_dictionary(commands_dictionary, query):
+    print(commands_dictionary)
 
-    main()
+    # Read key and value from dic
+    for k, val in commands_dictionary.items():
+        if query.lower() == val:
+            print('print value', k)
+            return k  # return command
+
+
+def exec_command(command):
+    try:
+        subprocess.Popen(command)
+        # TODO - Tidy this up and have it run a menu to give user more options not just re-run main()
+        return
+    except subprocess.CalledProcessError as error:
+        print('Subprocess error: ', error)
 
 
 def main():
     # Import the Speech Recogniser
     recogniser = sr.Recognizer()
 
-    application_dic = {}
     # Read in application names
-    application_dic = read_application_file()
+    commands_dictionary = read_command_file()
 
     # Import Audio File
+    # TODO - Testing files
     harvard = sr.AudioFile('audio_files/harvard.wav')
     jackhammer = sr.AudioFile('audio_files/jackhammer.wav')
 
     # Use Microphone
     mic = sr.Microphone()
 
-    # capture_from_audio(harvard, 0, 0, 1, False)  # Using decent, standard audio clip
-    # capture_from_audio(jackhammer, 0, 0, 0.5, True)  # Using poor quality, standard audio clip
-    audio_recognised = capture_from_mic(recogniser, mic, application_dic)
+    # Heading
+    print('===================================================='
+          '\n========== Application Speech Command =============='
+          '\n====================================================\n')
+    # Options
+    print('Please say what option you would like to use:\n1: Command from microphone\n2:Process an audio file'
+          '(Please only submit .wav files\n)')
 
-    print('Captured Audio: ', audio_recognised)
+    processed_input = capture_from_mic(recogniser, mic)
+    result = query_dictionary(commands_dictionary, processed_input)
+
+    if result == "capture_mic":
+        print('Please say the name of the application you wish to run\n')
+        request = capture_from_mic(recogniser, mic)
+        result = query_dictionary(commands_dictionary, request)
+        exec_command(result)  # execute command
+
+    elif result == "process_audio_file":
+        print('Please in the console specify a file path that is .wav format to be processed')
+        # TODO: Testing the file path is working when it is passed as a str
+        file_path = input()
+        request = process_audio_file(recogniser, file_path, 0, 0, 1, False)  # Using decent, standard audio clip
+
+
+# process_audio_file(harvard, 0, 0, 1, False)  # Using decent, standard audio clip
+# process_audio_file(jackhammer, 0, 0, 0.5, True)  # Using poor quality, standard audio clip
+# audio_recognised = capture_from_mic(recogniser, mic)
+
+# print('Captured Audio: ', audio_recognised)
 
 
 if __name__ == "__main__":
-    main_menu()
+    main()
